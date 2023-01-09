0c27d4ac8e4dfc5445ef1f943216c836e9c748dc
Sahin <103313877+itslilsahin@users.noreply.github.com>
2023-01-10 00:23:04 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "thread-spam",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "ravenn#0247",
+  "license": "ISC",
+  "dependencies": {
+    "discord.js-selfbot-v13": "^2.9.19"
+  }
+}
