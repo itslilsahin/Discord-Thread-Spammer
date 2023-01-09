1305d2a306213106b0300c67a6c10e3aea3b498b
Sahin <103313877+itslilsahin@users.noreply.github.com>
2023-01-10 00:37:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Client({intents:[32767],checkUpdate:false})
 
 client.on("messageCreate",async(message) => {
 if(message.content == "sahinthread" && message.author.id == "Owner ID") {
-for(var i=1;i<=70;i++){ 
+for(var i=1;i<=70;i++){  //70 yerine kaç tane açmasını istiyorsanız onu yazın
     message.guild.channels.cache.filter(x => x.type === "GUILD_TEXT").forEach(async(x) => await x.threads.create({name:"Thread Kanal İsmi"}))
 }
 //ravenn#0247
